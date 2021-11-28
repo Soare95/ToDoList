6562a52c12f1a22d2c2915b1e6b347d39dd53854
Soare95 <83647957+Soare95@users.noreply.github.com>
2021-11-28 16:06:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -136,6 +136,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen((process.env.PORT || 3000), function() {
   console.log("Server has started");
 });
